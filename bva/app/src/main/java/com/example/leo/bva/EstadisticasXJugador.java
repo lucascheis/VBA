9dc9fe59f3fe43f79c7b6aa01aa4c016282d2561
lucascheis <lucas.millo@gmail.com>
2016-08-10 17:23:34 -0300

    10/8

--- bva/app/src/main/java/com/example/leo/bva/EstadisticasXJugador.java
+++ bva/app/src/main/java/com/example/leo/bva/EstadisticasXJugador.java
@@ -6,6 +6,7 @@ package com.example.leo.bva;
 public class EstadisticasXJugador {
 
     public Integer Puntos;
+    public Integer IdJugador;
     public Integer Asistencias,Robos,Tapones,Perdidas,RebOff,RebDef,Faltas;
     public Integer TriplesMetidos,TriplesErrados,DoblesMetidos,DoblesErrados,TirosLibMetidos,TirosLibErrados;
 
